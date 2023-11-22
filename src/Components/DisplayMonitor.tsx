49c33b105816c4365a54a354ea654251fe8f437e
Robert Recalo <rrecalo3@ggc.edu>
2023-11-22 11:42:13 -0500

    Fix bug causing DisplayMonitor.tsx to not receive any verse

--- src/Components/DisplayMonitor.tsx
+++ src/Components/DisplayMonitor.tsx
@@ -1,13 +1,23 @@
+import { useEffect } from 'react'
 import Verse from '../types/Verse'
-export default function DisplayMonitor(verseToDisplay : Verse | undefined){
+
+type DisplayMonitorProps = {
+    verseToDisplay : Verse
+}
+
+export default function DisplayMonitor({verseToDisplay} : DisplayMonitorProps){
+
+    useEffect(()=>{
+        console.log(verseToDisplay);
+        },[verseToDisplay]);
 
     return (
         <>
         <div className="p-2">Monitoring</div>
            <div className="p-3">
-            <div id="display" className="text-2xl">{verseToDisplay?.text || ""}</div>
+            <div id="display" className="text-2xl">{verseToDisplay?.text}</div>
             <div className="font-bold text-sm">{verseToDisplay?.book_name || ""} 
-            {(verseToDisplay?.chapter || "") + ":" + (verseToDisplay?.number || "")}</div>
+            {" "}{(verseToDisplay?.chapter) + ":" + (verseToDisplay?.number)}</div>
         </div>
         </>
     )
