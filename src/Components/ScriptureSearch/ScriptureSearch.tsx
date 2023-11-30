bb2482e81921b9d3f65e08d073f6b01f6b84f296
Robert <robertrecalo@icloud.com>
2023-11-30 18:27:04 -0500

    change arrows in chapter selection buttons so they can center

--- src/Components/ScriptureSearch/ScriptureSearch.tsx
+++ src/Components/ScriptureSearch/ScriptureSearch.tsx
@@ -16,10 +16,11 @@ export default function ScriptureSearch({performSearch, currentBook, currentChap
             <ScriptureSearchBox performSearch={performSearch}/>
             <div className="flex flex-row justify-between items-center pe-2 py-1 border-black">
                 <div className="select-none cursor-default">{currentBook} {currentChapter}</div>
-                <div className="flex flex-row gap-2 justify-center items-center">
-                    <button className="outline-none hover:bg-neutral-200 dark:hover:bg-neutral-800 w-5 h-full" onClick={()=>{decrementChapter()}}>&#8592;</button>
+                <div className="flex flex-row gap-2 justify-center items-center select-none">
+                    <button className="outline-none hover:bg-neutral-200 dark:hover:bg-neutral-800 w-5 h-full
+                    flex flex-col justify-center items-center" onClick={()=>{decrementChapter()}}>{'<'}</button>
                     Chapter {currentChapter}
-                    <button className="outline-none hover:bg-neutral-200 dark:hover:bg-neutral-800 w-5 h-full" onClick={()=>{incrementChapter()}}>&#8594;</button>
+                    <button className="outline-none hover:bg-neutral-200 dark:hover:bg-neutral-800 w-5 h-full" onClick={()=>{incrementChapter()}}>{'>'}</button>
                 </div>
 
             </div>
