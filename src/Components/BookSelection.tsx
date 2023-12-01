d7e7343f5d4c0b63256ea341dfad349bf32e467a
Robert <robertrecalo@icloud.com>
2023-12-01 14:02:35 -0500

    Fix fetching from book list (missing arg)

--- src/Components/BookSelection.tsx
+++ src/Components/BookSelection.tsx
@@ -11,7 +11,7 @@ export default function BookSelection({bookName, activeBookName, openBook} : Boo
 
     return (
         <div className={` ps-1 font-light text-sm w-full pe-8 whitespace-nowrap select-none cursor-default ${activeBookName === bookName ? 'bg-neutral-300 dark:bg-neutral-800 dark:text-neutral-50' : 'dark:bg-neutral-900 dark:text-neutral-400'}`}
-        onClick={()=>{bookName.startsWith("Song") ? openBook("song") : openBook(bookName)}}
+        onClick={()=>{bookName.startsWith("Song") ? openBook("song", true) : openBook(bookName, true)}}
         >{bookName}</div>
     )
 }
